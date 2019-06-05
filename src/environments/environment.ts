4f4913d4e41b1720a4abd9bf6b47685b73455757
Mritunjai Pratap Singh <Mritunjai_PratapSing@Dell.com>
2019-06-05 13:13:06 +0530

    changed env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'http://52.14.178.44:3000/api/',
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'http://52.14.178.44:3000/api/',
+  //apiUrl: 'http://localhost:3000/api/',
 };
 
 /*
